4dedfbc34122b860af772830c1bbbf72b8fff11b
97kos <kondmo@gmail.com>
2018-04-30 11:59:38 +0200

    Changes

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -3,7 +3,9 @@ package com.crud.tasks;
 import com.crud.tasks.controller.TrelloController;
 import com.crud.tasks.domain.Task;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
