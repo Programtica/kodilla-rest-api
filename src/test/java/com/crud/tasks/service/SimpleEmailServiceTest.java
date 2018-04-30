11a021451c869b4d9502c0c774c05fdee2d201a8
97kos <kondmo@gmail.com>
2018-04-30 17:45:11 +0200

    Zadanie 19.4

--- src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -24,7 +24,7 @@ public class SimpleEmailServiceTest {
     @Test
     public void shouldSendEmail() {
         //Given
-        Mail mail = new Mail("test@test.com", "Test", "TestMessage", "kondmo@gmail.com");
+        Mail mail = new Mail("kontakt@mryndak.pl", "Test", "TestMessage", "kondmo@gmail.com");
 
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
