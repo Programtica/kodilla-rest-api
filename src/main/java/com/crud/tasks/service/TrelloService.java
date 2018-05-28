a46bf95dbc508a302e4c06438fc24bf7578a9701
97kos <kondmo@gmail.com>
2018-05-28 14:03:38 +0200

    Zadanie 24.2

--- src/main/java/com/crud/tasks/service/TrelloService.java
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -36,7 +36,7 @@ public class TrelloService {
 
         ofNullable(newCard).ifPresent(card -> emailService.send(new Mail(adminConfig.getAdminMail(),
                 SUBJECT,
-                "New card: " + trelloCardDto.getName() + "has been created on Your Trello account")));
+                "New card: " + trelloCardDto.getName() + " has been created on Your Trello account!")));
 
         return newCard;
     }
